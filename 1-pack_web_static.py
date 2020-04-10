247757d5844e304ba1313f3e54ef63631c375f59
Thorlak2202 <darkdrakkar@gmail.com>
2020-04-10 17:42:47 -0500

    pep8 compliance

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,13 +8,14 @@ from fabric.api import *
 from datetime import datetime
 import os
 
+
 def do_pack():
     try:
         if not os.path.exists("versions"):
             local('mkdir -p versions')
         curr_time = datetime.now().strftime("%Y%m%d%H%M%S")
         outpt = local('tar -czvf versions/web-static_{}.tgz web_static'.
-                    format(curr_time))
+                      format(curr_time))
         return outpt
     except:
         return None
